e86cd1c5700d7480d7910177af1eea4460428866
Silas Matos <98833867+SilasMatos@users.noreply.github.com>
2024-09-22 21:26:47 -0300

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,9 @@
 import PlusIcon from '../icons/PlusIcon'
-import { useAuth } from '../contexts/AuthContext'
 
 function Navbar({ addColumn: addColumn }: { addColumn: () => void }) {
-  const { logout } = useAuth()
-
-  const handleLogout = () => {
-    logout()
-  }
   return (
     <div className="flex justify-between w-full items-center mb-20 bg-[#111111] py-4 px-2">
-      <div className="font-semibold">Kanban Project</div>
+      <div className="font-semibold ">Kanban Project</div>
       <button
         onClick={addColumn}
         type="button"
@@ -19,7 +13,6 @@ function Navbar({ addColumn: addColumn }: { addColumn: () => void }) {
         <PlusIcon size={10} />
       </button>
       <button
-        onClick={handleLogout}
         type="button"
         className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
       >
